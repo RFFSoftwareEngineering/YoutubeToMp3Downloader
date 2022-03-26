7f6dac430da3c588e4973b958b114d3df46e3dc1
Rafael F Furlan <RFFSoftwareEngineering@outlook.com>
2022-03-26 16:42:40 -0300

    Adicionar arquivos de projeto.

---
+++ YoutubeToMp3Downloader/YoutubeToMp3Downloader.py
@@ -0,0 +1,21 @@
+from pytube import Playlist, YouTube
+from tqdm import tqdm
+import os
+
+
+playlist = Playlist('https://www.youtube.com/watch?v=YNB6aIwC8mg&list=PLrxByE8f6bAkMOm721jL91JxPPwDDmrsz&index=1')
+print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
+
+
+for video in tqdm(playlist.videos):
+    try:
+        video = video.streams.filter(only_audio=True).first()
+        out_file = video.download()
+
+        base, ext = os.path.splitext(out_file)
+        new_file = base + ".mp3"
+        os.rename(out_file, new_file)
+    except:
+        pass
+
+print("Done!")
