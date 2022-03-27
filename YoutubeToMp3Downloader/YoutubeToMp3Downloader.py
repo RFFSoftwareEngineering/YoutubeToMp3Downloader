79823f2c0439a71d78d423f3353da838e150a3e4
Rafael F Furlan <RFFSoftwareEngineering@outlook.com>
2022-03-27 19:17:19 -0300

    good!, now with single song option later let's download a 1500 videos playlist to mp3 files

--- YoutubeToMp3Downloader/YoutubeToMp3Downloader.py
+++ YoutubeToMp3Downloader/YoutubeToMp3Downloader.py
@@ -2,7 +2,7 @@ from pytube import Playlist, YouTube
 from tqdm import tqdm
 import os
 
-
+# for playlists:
 playlist = Playlist('https://www.youtube.com/watch?v=YNB6aIwC8mg&list=PLrxByE8f6bAkMOm721jL91JxPPwDDmrsz&index=1')
 print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
 
@@ -19,3 +19,14 @@ for video in tqdm(playlist.videos):
         pass
 
 print("Done!")
+
+
+# for single song:
+"""
+video = YouTube('https://www.youtube.com/watch?v=FTB1b228h8g')
+video = video.streams.filter(only_audio=True).first()
+out_file = video.download()
+base, ext = os.path.splitext(out_file)
+new_file = base + ".mp3"
+os.rename(out_file, new_file)
+"""
